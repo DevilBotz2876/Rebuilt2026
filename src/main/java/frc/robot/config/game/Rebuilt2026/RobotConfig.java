fd571a2f9ef9fe8a73f668074c1178ea3bad98f0
Ethan Porter <emanporter2009@gmail.com>
2025-10-29 21:12:56 -0400

    removed Vision io, controls, and implementation

--- src/main/java/frc/robot/config/game/Rebuilt2026/RobotConfig.java
+++ src/main/java/frc/robot/config/game/Rebuilt2026/RobotConfig.java
@@ -1,12 +1,6 @@
 package frc.robot.config.game.Rebuilt2026;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -14,14 +8,12 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Robot;
 import frc.robot.subsystems.implementations.drive.DriveBase;
-import frc.robot.subsystems.implementations.vision.VisionSubsystem;
-import frc.robot.subsystems.interfaces.Vision.Camera;
 
 /* Put all constants here with reasonable defaults */
 public class RobotConfig {
   public static DriveBase drive;
   public static SendableChooser<Command> autoChooser;
-  public static VisionSubsystem vision;
+  // TODO: Add VisionSubsystem Declaration
 
   // Controls
   public CommandXboxController mainController = new CommandXboxController(0);
@@ -37,37 +29,23 @@ public class RobotConfig {
       autoChooser.setDefaultOption("No Auto Routines Specified", Commands.none());
     }
 
-    vision =
-        new VisionSubsystem(AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark));
+    // TODO: Add VisionSubsystem Initialization
 
     if (stubVision) {
-      if (Robot.isSimulation()) {
-        vision.addCamera(
-            new Camera(
-                "rear_cam",
-                new Transform3d(
-                    new Translation3d(
-                        Units.inchesToMeters(0.295),
-                        Units.inchesToMeters(-11.443),
-                        Units.inchesToMeters(39.663)),
-                    new Rotation3d(
-                        Units.degreesToRadians(12),
-                        Units.degreesToRadians(-33),
-                        Units.degreesToRadians(170)))));
-      }
+      // TODO: Add VisionSubsystem Settings
     }
   }
 
   public void configureBindings() {
     if (Robot.isSimulation()) {
-      vision.enableSimulation(() -> RobotConfig.drive.getPose(), true);
+      // TODO: Add VisionSubsystem Simulation Support
 
       // HACK just to verify autos are visible without connecting to robot
       RobotConfig.autoChooser = AutoBuilder.buildAutoChooser("Sit Still");
     }
 
     // Send vision-based odometry measurements to drive's odometry calculations
-    vision.setVisionMeasurementConsumer(drive::addVisionMeasurement);
+    // vision.setVisionMeasurementConsumer(drive::addVisionMeasurement);
 
     if (null != RobotConfig.autoChooser) {
       SmartDashboard.putData("Autonomous", RobotConfig.autoChooser);
