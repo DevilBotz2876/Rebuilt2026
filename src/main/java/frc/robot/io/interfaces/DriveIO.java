f898da5fa11e7e6fbf5640c3fee26ea6fb1187fc
Ethan Porter <emanporter2009@gmail.com>
2026-01-12 15:49:09 -0500

    uncomment pathplanner code

--- src/main/java/frc/robot/io/interfaces/DriveIO.java
+++ src/main/java/frc/robot/io/interfaces/DriveIO.java
@@ -3,8 +3,8 @@ package frc.robot.io.interfaces;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.swerve.SwerveModule;
-// TODO: Add Pathplanner back
-// import com.pathplanner.lib.util.FlippingUtil;
+
+import com.pathplanner.lib.util.FlippingUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
@@ -60,11 +60,10 @@ public class DriveIO {
     inputs.poseX = inputs.pose.getTranslation().getX();
     inputs.poseY = inputs.pose.getTranslation().getY();
     inputs.poseRotInDegrees = inputs.pose.getRotation().getDegrees();
-    // TODO:Flip Pose
-    // inputs.flippedPose = FlippingUtil.flipFieldPose(inputs.pose);
-    // inputs.flippedPoseX = inputs.flippedPose.getTranslation().getX();
-    // inputs.flippedPoseY = inputs.flippedPose.getTranslation().getY();
-    // inputs.flippedPoseRotInDegrees = inputs.flippedPose.getRotation().getDegrees();
+    inputs.flippedPose = FlippingUtil.flipFieldPose(inputs.pose);
+    inputs.flippedPoseX = inputs.flippedPose.getTranslation().getX();
+    inputs.flippedPoseY = inputs.flippedPose.getTranslation().getY();
+    inputs.flippedPoseRotInDegrees = inputs.flippedPose.getRotation().getDegrees();
 
     for (int i = 0; i < 4; i++) {
       SwerveModule<TalonFX, TalonFX, CANcoder> module = drivetrain.getModule(i);
