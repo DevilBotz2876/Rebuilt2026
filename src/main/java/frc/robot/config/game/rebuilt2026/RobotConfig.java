45cc4490fc711f68b2324f2798c2c4db73010bed
Ethan Porter <emanporter2009@gmail.com>
2026-01-29 18:54:18 -0500

    add camera and real map

--- src/main/java/frc/robot/config/game/rebuilt2026/RobotConfig.java
+++ src/main/java/frc/robot/config/game/rebuilt2026/RobotConfig.java
@@ -46,7 +46,7 @@ public class RobotConfig {
     // no-args constructor, for now
     vision =
         new VisionSubsystem(
-            AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark),
+            AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark),
             Optional.of(drive::addVisionMeasurement));
 
     CameraSettings simCameraSettings = new CameraSettings();
@@ -54,88 +54,88 @@ public class RobotConfig {
     simCameraSettings.resWidth = 800;
     simCameraSettings.resHeight = 600;
     // "sim" camera that +12 inch along x axis and pitch of -45 degrees
-    vision.addCamera(
-        new CameraPhoton(
-            "left",
-            new Transform3d(
-                new Translation3d(
-                    Units.inchesToMeters(10), Units.inchesToMeters(14.5), Units.inchesToMeters(7)),
-                new Rotation3d(
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(5, Degrees),
-                    Angle.ofBaseUnits(-22, Degrees))),
-            simCameraSettings,
-            vision.getFieldLayout()));
+    // vision.addCamera(
+    //     new CameraPhoton(
+    //         "left",
+    //         new Transform3d(
+    //             new Translation3d(
+    //                 Units.inchesToMeters(10.75), Units.inchesToMeters(14.5), Units.inchesToMeters(7 + 35)),
+    //             new Rotation3d(
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(5, Degrees),
+    //                 Angle.ofBaseUnits(-22 + 180, Degrees))),
+    //         simCameraSettings,
+    //         vision.getFieldLayout()));
     vision.addCamera(
         new CameraPhoton(
             "front",
             new Transform3d(
                 new Translation3d(
                     Units.inchesToMeters(14.5),
-                    Units.inchesToMeters(0.25),
-                    Units.inchesToMeters(3.5)),
-                new Rotation3d(
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees))),
-            simCameraSettings,
-            vision.getFieldLayout()));
-    vision.addCamera(
-        new CameraPhoton(
-            "right",
-            new Transform3d(
-                new Translation3d(
-                    Units.inchesToMeters(10), Units.inchesToMeters(-14.5), Units.inchesToMeters(7)),
+                    Units.inchesToMeters(0.0),
+                    Units.inchesToMeters(1.765+2+ 18)),
                 new Rotation3d(
                     Angle.ofBaseUnits(0, Degrees),
                     Angle.ofBaseUnits(5, Degrees),
-                    Angle.ofBaseUnits(22, Degrees))),
-            simCameraSettings,
-            vision.getFieldLayout()));
-    vision.addCamera(
-        new CameraPhoton(
-            "other1",
-            new Transform3d(
-                new Translation3d(0, 0, 0),
-                new Rotation3d(
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees),
                     Angle.ofBaseUnits(0, Degrees))),
             simCameraSettings,
             vision.getFieldLayout()));
-    vision.addCamera(
-        new CameraPhoton(
-            "other2",
-            new Transform3d(
-                new Translation3d(0, 0, 0),
-                new Rotation3d(
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees))),
-            simCameraSettings,
-            vision.getFieldLayout()));
-    vision.addCamera(
-        new CameraPhoton(
-            "other3",
-            new Transform3d(
-                new Translation3d(0, 0, 0),
-                new Rotation3d(
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees))),
-            simCameraSettings,
-            vision.getFieldLayout()));
-    vision.addCamera(
-        new CameraPhoton(
-            "other4",
-            new Transform3d(
-                new Translation3d(0, 0, 0),
-                new Rotation3d(
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees),
-                    Angle.ofBaseUnits(0, Degrees))),
-            simCameraSettings,
-            vision.getFieldLayout()));
+    // vision.addCamera(
+    //     new CameraPhoton(
+    //         "right",
+    //         new Transform3d(
+    //             new Translation3d(
+    //                 Units.inchesToMeters(10), Units.inchesToMeters(-14.5), Units.inchesToMeters(7 + 35)),
+    //             new Rotation3d(
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(5, Degrees),
+    //                 Angle.ofBaseUnits(22 + 180, Degrees))),
+    //         simCameraSettings,
+    //         vision.getFieldLayout()));
+    // vision.addCamera(
+    //     new CameraPhoton(
+    //         "other1",
+    //         new Transform3d(
+    //             new Translation3d(0, 0, 0),
+    //             new Rotation3d(
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees))),
+    //         simCameraSettings,
+    //         vision.getFieldLayout()));
+    // vision.addCamera(
+    //     new CameraPhoton(
+    //         "other2",
+    //         new Transform3d(
+    //             new Translation3d(0, 0, 0),
+    //             new Rotation3d(
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees))),
+    //         simCameraSettings,
+    //         vision.getFieldLayout()));
+    // vision.addCamera(
+    //     new CameraPhoton(
+    //         "other3",
+    //         new Transform3d(
+    //             new Translation3d(0, 0, 0),
+    //             new Rotation3d(
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees))),
+    //         simCameraSettings,
+    //         vision.getFieldLayout()));
+    // vision.addCamera(
+    //     new CameraPhoton(
+    //         "other4",
+    //         new Transform3d(
+    //             new Translation3d(0, 0, 0),
+    //             new Rotation3d(
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees),
+    //                 Angle.ofBaseUnits(0, Degrees))),
+    //         simCameraSettings,
+    //         vision.getFieldLayout()));
   }
 
   public RobotConfig(boolean stubDrive, boolean stubAuto, boolean stubVision) {
